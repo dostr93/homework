855d49c8cf3e87583c1777de32036e827a38f13f
My Name <myEmail@example.com>
2021-08-05 17:49:17 +0300

    upload hw3

--- app.js
+++ app.js
@@ -1,80 +1,72 @@
-// Task 1
+// // Task 1 
 
-const citiesAndCountries = {
-	'Киев': 'Украина',
-	'Нью-Йорк': 'США',
-	'Амстердам': 'Нидерланды',
-	'Берлин': 'Германия',
-	'Париж': 'Франция',
-	'Лиссабон': 'Португалия',
-	'Вена': 'Австрия',
-};
-
-let arr = [];
-for (let property in citiesAndCountries) {
-    arr.push(property + ' - ' + citiesAndCountries[property])
+function Employee(employee) {
+	this.id = employee.id;
+	this.name = employee.name;
+	this.surname = employee.surname;
+	this.salary = employee.salary;
+	this.workExperience = employee.workExperience;
+	this.isPrivileges = employee.isPrivileges;
+	this.gender = employee.gender;
 }
-console.log(arr);
+
+const employeeObj = new Employee(employeeArr[0]);
 
 // Task 2 
-let r = 9;
-const arrInArr = () => {
-	const mainArr = []
-	let insideArr = [];
-	for (let i = 1; i <= r; i++) {
-		if(i % 3 === 0) {
-			insideArr[insideArr.length] = i;
-			mainArr[mainArr.length] = insideArr;
-			insideArr = [];
-			continue
-		}
-		insideArr[insideArr.length] = i;
-	}
-	return mainArr;
-} 
-	console.log(arrInArr());
 
-
-// Task 3
-
-const namesOfDays = {
-    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+Employee.prototype.getFullName = function() {
+	return `${this.surname} ${this.name}`; 
 }
 
-const lang = 'ru';
-const day = 7;
+console.log(employeeObj.getFullName());
 
-const getNameOfDay = (lang, day) => namesOfDays[lang][day - 1];
+// Task 3 
 
-// getNameOfDay();
+let createEmployesFromArr = (arr) => {
+	return arr.map(item => {
+		return new Employee(item)
+	});
+};
+    
+const employeeConstructArr = createEmployesFromArr(employeeArr)
 
 // Task 4 
 
-let b = [12, 898, 899, 900];
-let min1 = b[0];
-let min2 = b[1];
-
-const sumOfMin = () => {
-	for (let i = 2; i < b.length; i++) {
-		if (b[i] < min1) {
-			min1 = b[i];
-		} else {
-			if (b[i] < min2) {
-				min2 = b[i]
-			}
-		}
-	}
-	return min1 + min2
+const getFullNamesFromArr = (arr) => {
+	return arr.map(item => {
+		return item.getFullName()
+	})
 }
+	
+getFullNamesFromArr(employeeConstructArr) 
 
-// Task 5 
-const a = (arr) => {
-	let result = 0;
-	let counter = 1;
-	for (let i = arr.length; i > 0; i--) {
-		arr[i] === 1 ? result += counter : result;
-		counter *= 2
-	}
-	return result
-}
+// Task 5
+
+const getMiddleSalary = (arr) => {
+	const totalSalary = arr.map(item => item.salary).reduce((prev, item) => (prev + item));
+	const middleSalary = totalSalary / arr.length;
+	return Math.round(middleSalary);
+};
+
+getMiddleSalary(employeeConstructArr);
+
+// Task 6 
+
+const getRandomEmployee = (arr) => {
+	const randomEmployee = Math.floor(Math.random() * arr.length);
+	return arr[randomEmployee];
+};
+	
+getRandomEmployee(employeeConstructArr);
+
+//Task 7
+
+// Создаем экземпляр на основе объекта 
+// который берем из массива по 0 индексу
+
+const employeeObjTask7 = new Employee(employeeArr[0]);
+
+
+employeeObjTask7.fullInfo
+// Результат
+//  id - 1, name - Денис, surname - Хрущ
