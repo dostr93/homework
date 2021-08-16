b6c7eeb3cf344cf8c360b2f72ee1af6790ebe561
My Name <myEmail@example.com>
2021-08-16 18:07:25 +0300

    upload hw5

--- app.js
+++ app.js
@@ -1,99 +1,63 @@
-//  Task 1 
-
-
-class Student {
-	constructor(enrollee) {
-		this.id = Student.getId();
-		this.name = enrollee.name;
-		this.surname = enrollee.surname;
-		this.raitingPoint = enrollee.raitingPoint;
-		this.schoolPoint = enrollee.schoolPoint;
-		this.isSelfPayment = Student.getPayment(this);
-	}
-	
-	static listOfStudents = [];
-
-	static sortOfStudents(arr) {
-		return arr.sort((a, b) => {
-			if (b.ratingPoint === a.ratingPoint) {
-				return b.schoolPoint - a.schoolPoint;
-			} else {
-				return b.ratingPoint - a.ratingPoint
-			};
-		});	
-	};
-
-	static id = 1; 
-
-	static getId() {
-		return this.id++
-	}
-
-	static getPayment(student) {
-		this.sortOfStudents(this.listOfStudents);
-		this.listOfStudents.push(student);
-		
-		for (let i = 0; i < this.listOfStudents.length; i++) {
-			if (i <= 4 && this.listOfStudents[i].ratingPoint >= 800) {
-				this.listOfStudents[i].isSelfPayment = true;
-            } else {
-                this.listOfStudents[i].isSelfPayment = false;
-            }
-        }
-
-        return true;
+// Task 1
+function f1 () {
+    let count = 0;
+    return function(value) {
+        count += value;
+        console.log(count)
     }
-    
-    get listOfStudents() {
-        return this.listOfStudents;
-    }
-};
-
-for (let i = 0; i < studentArr.length; i++) {
-	new Student(studentArr[i])
 }
+const counter = f1();
 
-// Task 2 
-
-class CustomString {
-
-	constructor(word) {
-		this.word = word
-	}
-	
-	reverse(word) {
-		let reverseResult = '';
-		for (let i = word.length - 1; i >= 0; i--) {
-			reverseResult += word[i]
+// Task 2
+function f2 () {
+    let arr = [];
+    return function (value) {
+        if (value == undefined) {
+			arr.length = 0;
+			return arr;
+		} else {
+			arr.push(value)
+			return arr
 		}
+    }
+}
+const getUpdaterArr = f2();
+getUpdaterArr();
 
-		return reverseResult;
-	}	
+// Task 3 
 
-	ucFirst(word) {
-		let uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-		let lowcase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-		let letter = '';
-
-		
-
-		for (let i = 0; i < lowcase.length; i++) {
-			if (word[0] == lowcase[i]) {
-				letter = uppercase[i];
-			}
-		} return letter + word.slice(1);
-	} 
-
-	ucWords(words) {
-		let firstLetters = '';
-		for (let i = 0; i < words.length; i++) {
-			if (words[i - 1] === ' ' || i === 0) {
-				firstLetters += words[i].toUpperCase();
-			} else {
-				firstLetters += words[i]
-			}
-		} return firstLetters;
+function f3 () {
+	let secondsStart = 0
+	return function () {
+		if (secondsStart === 0) {
+			secondsStart = Math.floor(Date.now() / 1000)
+			return 'enabled';
+		}
+		let secondsEnd = Math.floor(Date.now() / 1000);
+		let timeForDoSomethisng = secondsEnd - secondsStart;
+		secondsStart = secondsEnd;
+		return timeForDoSomethisng
 	}
 }
 
-const myString = new CustomString();
+const getTime = f3();
+getTime();
+
+// Task 4
+
+let time = 5;
+let stopInterval = setInterval(timer, 1000);
+
+function timer() {
+	let minutes = Math.floor(time / 60);
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	let seconds = time % 60;
+	seconds = seconds < 10 ? "0" + seconds : seconds;
+	time--;
+	if (time < 0) {
+		clearInterval(stopInterval);
+		console.log('Time End');
+	} else {
+		console.log(`${minutes} : ${seconds}`);
+	}
+}
