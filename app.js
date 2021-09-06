509c9dfa5bf1370d20d383e4da483441913a4551
My Name <myEmail@example.com>
2021-09-06 19:15:45 +0300

    upload hw9

--- app.js
+++ app.js
@@ -1,151 +1,88 @@
+Task1
 
+const enteredNumber = prompt('Enter the number');
+const button = document.querySelector('.button');
 
-
-const arr = ['Vasya', 'Petya', 'Alexey'];
-
-function removeUser(array, index) {
-    return array.splice(index, 1);
-}
-
-removeUser(arr, 0)
-
-console.log(arr);
-
-// Task2
-
-const obj = { name: 'Vasya', age: 1}
-const arr2 = [];
-function getAllKeys(obj) {
-    for (let key in obj) {
-        arr2.push(key)
-    } 
-    return arr2;
+for (i = 1; i <= Number(enteredNumber); i++) {
     
-}
+    let input = document.createElement('input')
+    input.value = `Input ${i}`;
 
-getAllKeys(obj);
-
-console.log(arr2);
-
-// Task 3
-
-const obj3 = { name: 'Vasya', age: 1}
-const arr3 = [];
-function getAllValues(obj3) {
-    for (let key in obj3) {
-        arr3.push(obj3[key])
-    } 
-    return arr3;
     
+    if (!(i % 3)) input.value = 'Some text';
+    input.classList.add('input-item');
+    
+    button.before(input);
+    
+    if (enteredNumber === i) {
+        input.classList.add()
+    }
+    
+    if (!(i % 2 )) {
+        input.classList.add('input-background');
+    }
+
+    if (i === Number(enteredNumber)) input.classList.add('margin-zero');
 }
 
-getAllValues(obj3);
+// Task 2
 
-console.log(arr3);
+const newDiv = document.createElement('div')
+const newTag = document.createElement('h2');
+const startBtn = document.createElement('button')
+const stopBtn = document.createElement('button')
+startBtn.innerText = 'Start';
+stopBtn.innerText = 'Stop';
+
+document.forms[0].after(newDiv);
+newDiv.prepend(startBtn, stopBtn);
+newDiv.append(newTag)
+
+startBtn.addEventListener('click', () => {
+    Id = setInterval(timer, 1000);
+    return
+});
+stopBtn.addEventListener('click', () => clearInterval(Id));
+    
+let timer = function() {
+    const time = document.querySelector('h2')
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if (hours < 10) hours = '0' + hours;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+
+    time.textContent = hours + ':' + minutes + ':' + seconds;
+}
+
+// Task 3 
+
+function colorRed(){
+	const lastParagraph = document.body.querySelector('#main').lastElementChild;
+	lastParagraph.style.backgroundColor = 'red';
+	return
+}
+
+function footerDown(){
+	const footer = document.querySelector('#footer')
+	const wrapper = document.querySelector('#wrapper')
+	wrapper.append(footer)
+	return
+}
+
+colorRed()
+footerDown()
+
 
 // Task 4
 
-
-// Task6
-function getCompanyName() {
-    const companiesArr = [];
-    let elemOfArr = '';
-    for (let i = 0; i < condidateArr.length; i++) {
-        elemOfArr = condidateArr[i];
-        companiesArr.push(elemOfArr.company)
-    }
-
-    let uniqueCompany = new Set(companiesArr)
-    const uniqueCompanyArr = [...uniqueCompany]
-    return uniqueCompanyArr
-}
-
-
-
-// Task 7
-function getUserByYear(year) {
-    let elemOfArr = '';
-    let registeredDate = '';
-    const userByIdArr= [];
-    for (let i = 0; i < condidateArr.length; i++) {
-        elemOfArr = condidateArr[i];
-        registeredDate = elemOfArr.registered;
-        if (registeredDate.includes(year)) {
-            userByIdArr.push(elemOfArr["_id"]);
-        }
-    }
-    return userByIdArr;
-} 
-
-// Task 8
-
-class Candidate {
-    constructor(candidates) {
-        this.id = candidates["_id"];
-        this.index = candidates.index;
-        this.guid = candidates.guid;
-        this.isActive = candidates.isActive;
-        this.balance = candidates.balance;
-        this.picture = candidates.picture;
-        this.age = candidates.age;
-        this.eyeColor = candidates.eyeColor;
-        this.name = candidates.name;
-        this.gender = candidates.gender;
-        this.company = candidates.company;
-        this.email = candidates.email;
-        this.phone = candidates.phone;
-        this.address = candidates.address;
-        this.about = candidates.about;
-        this.registered = candidates.registered;
-        this.latitude = candidates.latitude;
-        this.longitude = candidates.longitude
-        this.tags = candidates.tags;
-        this.friends = candidates.friends;
-        this.greeting = candidates.greeting
-        this.favoriteFruit = candidates.favoriteFruit;
-    }
-
-    // Task 5 
-
-    state() {
-        let elem = 0
-        let addressArr = []
-        for (let i = 0; i < condidateArr.length; i++) {
-            elem = condidateArr[i];
-            addressArr.push(elem.address.split(', ')[2]);
-        }
-        return addressArr;
-    }
-}
-        
-  
-const condidate = new Candidate(condidateArr[1])
+const INGREDIENTS = {
+    "cocoa": ["cocoa powder", "milk", "sugar"],
+    "cappuccino": ["milk", "coffee"],
+    "smoothie": ["banana", "orange", "sugar"],
+    "matcha frappe": ["matcha", "milk", "ice"]
+    };
     
-function getCondidatesByUnreadMsg(message) {
-    const messageArr = [];
-    let unreadMessage ='';
-    for (let i = 0; i < condidateArr.length; i++) {
-        const candidate = new Candidate(condidateArr[i]);
-        unreadMessage = candidate.greeting;
-        if (unreadMessage.includes(message)) {
-            messageArr.push(candidate)
-        }
-    }
-    return messageArr;
-}
-
-// Task9
-function getCondidatesByGender(gender) {
-    const genderArr = [];
-    for (let i = 0; i < condidateArr.length; i++) {
-        const candidate = new Candidate(condidateArr[i]);
-        let typeOfGender = candidate.gender;
-        if (typeOfGender === gender) {
-            genderArr.push(candidate)
-        }
-    }
-    return genderArr;
-}
-
-
-// Task 10
